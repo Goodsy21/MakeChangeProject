4a890ad50d626c94ba43e3a376a57f1a3edf9135
Ryan Goode <goode21@gmail.com>
2022-10-22 19:20:46 -0600

    take2

--- src/App/CashRegister.java
+++ src/App/CashRegister.java
@@ -36,8 +36,9 @@ public class CashRegister {
 			Double.parseDouble(String.format("%,.2f", change));
 			System.out.printf("Your change is $%,.2f", change);
 			
-			hundreds = (int) (change / 100);
+			hundreds = (int) (change / 100); 
 			change = change % 100;
+			//I coded this even though I won't accept payment that is more than $100 over the total price
 			
 			fifties = (int) (change / 50);
 			change = change % 50;
@@ -113,7 +114,6 @@ public class CashRegister {
 			} else
 				System.out.println("\n" + quarters + " quarters");
 		}
-
 		if (dimes > 0) {
 			if (dimes == 1) {
 				System.out.println("\n1 dime");
@@ -141,45 +141,3 @@ public class CashRegister {
 	}
 
 }
-//			if (cashPaid > price) {
-//				System.out.println("");
-//			}
-//		else {
-////			System.out.println(change);
-////
-////		}
-//	private static void totalDue() {
-//		
-//		
-//	}
-
-//	private static void giveChange() {
-//		cashPaid();
-//
-//	}
-//In the cash register we will calculate the amount of change returned to a customer based on the purchase price and the amount tendered. We will also notify the attendant how many of each piece of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer. Change should be provided using the largest bill and coin denominations as possible. Denominations that are not used should not be displayed.
-//
-//Hint: Mod operator
-//
-//User Story #1
-//The user is prompted asking for the price of the item.
-//
-//User Story #2
-//The user is then prompted asking how much money was tendered by the customer.
-//
-//User Story #3
-//Display an appropriate message if the customer provided too little money or the exact amount.
-//
-//User Story #4
-//If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
-//
-//Grading
-//This is a graded project. You are expected to have your project completed by the start of class on Monday morning.
-//
-//You will be given either a pass or fail based on whether your code works given all of the following test conditions:
-//
-//Amount: .67, Tendered: .50, Result: Error message
-//Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
-//Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
-//Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
-//Amount: any amount less than 20.00, Tendered: anything greater than amount: correct denominations for correct change.
